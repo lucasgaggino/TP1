eaed7fea48dd383b22e8f0a7412909c80abf3c34
lucasgaggino <lucas.gaggino@gmail.com>
2019-09-16 18:07:43 -0300

    Compilacion condicional tres Archivos

--- src/TP1.c
+++ src/TP1.c
@@ -43,13 +43,30 @@
 /*==================[macros and definitions]=================================*/
 #define COMPILAR_CODIGO_BLINKY 1
 #define COMPILAR_CODIGO_SWITCH_LEDS 2
-#define COMPILAR_CODIGO COMPILAR_CODIGO_SWITCH_LEDS
+#define COMPILAR_CODIGO_TICKHOOK 3
+
+
+#define COMPILAR_CODIGO COMPILAR_CODIGO_TICKHOOK
 
 
 
 /*==================[internal data declaration]==============================*/
 
 /*==================[internal functions declaration]=========================*/
+void myTickHook( void *ptr ){
+
+    static bool_t ledState = OFF;
+
+    gpioMap_t led = (gpioMap_t)ptr;
+
+    if( ledState ){
+       ledState = OFF;
+    }
+    else{
+       ledState = ON;
+    }
+    gpioWrite( led, ledState );
+ }
 
 /*==================[internal data definition]===============================*/
 
@@ -67,6 +84,7 @@ int main(void){
 
    /* ------------- INICIALIZACIONES ------------- */
 #if COMPILAR_CODIGO==COMPILAR_CODIGO_BLINKY
+
    /* Inicializar la placa */
    boardConfig();
 
@@ -90,7 +108,7 @@ int main(void){
    return 0 ;
 }
 
-#elif  COMPILAR_CODIGO==COMPILAR_CODIGO_SWITCH_LEDS
+#elif COMPILAR_CODIGO==COMPILAR_CODIGO_SWITCH_LEDS
 
 /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
 
@@ -130,7 +148,57 @@ int main(void){
    /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
       por ningun S.O. */
    return 0 ;
+ }
+#elif COMPILAR_CODIGO == COMPILAR_CODIGO_TICKHOOK
+
+
+ /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+
+
+    /* ------------- INICIALIZACIONES ------------- */
+
+    /* Inicializar la placa */
+    boardConfig();
+
+    /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+       periodicamente una interrupcion cada 50ms que incrementa un contador de
+       Ticks obteniendose una base de tiempos). */
+    tickConfig( 50 );
+
+    /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+       simplemente una funcion que se ejecutara peri­odicamente con cada
+       interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+       a una interrupcion.
+       El segundo parametro es el parametro que recibe la funcion myTickHook
+       al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+    */
+    tickCallbackSet( myTickHook, (void*)LEDR );
+    delay(1000);
+
+    /* ------------- REPETIR POR SIEMPRE ------------- */
+    while(1) {
+       tickCallbackSet( myTickHook, (void*)LEDG );
+       delay(1000);
+       tickCallbackSet( myTickHook, (void*)LEDB );
+       delay(1000);
+       tickCallbackSet( myTickHook, (void*)LED1 );
+       delay(1000);
+       tickCallbackSet( myTickHook, (void*)LED2 );
+       delay(1000);
+       tickCallbackSet( myTickHook, (void*)LED3 );
+       delay(1000);
+       tickCallbackSet( myTickHook, (void*)LEDR );
+       delay(1000);
+    }
+
+    /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+       por ningun S.O. */
+    return 0 ;
+ }
+
 
 #endif
-   }
+
+ /* FUNCION que se ejecuta cada vez que ocurre un Tick. */
+
 /*==================[end of file]============================================*/
